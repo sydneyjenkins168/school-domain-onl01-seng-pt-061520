fa9bb9c5d53f8b6861eba5c48fdfe42ca65b5593
Sydney Jenkins <sydjenkins168@gmail.com>
2020-07-07 01:37:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,14 @@
 class School
-  attr_reader :name, :roster
+  attr_reader :name, :roster, :add_student
    
    def initialize(name)
      @name=name
      @roster = {}
    end 
+   
+   def add_student=(student, grade)
+     
+   
 
   
   

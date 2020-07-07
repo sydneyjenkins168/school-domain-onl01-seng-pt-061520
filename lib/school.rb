65d01dcbf67e6b9d59fa8d8277aa59b2c08cd400
Sydney Jenkins <sydjenkins168@gmail.com>
2020-07-07 01:42:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,20 +1,30 @@
 class School
-  attr_accessor :name, :roster, :grade
-  attr_reader :school
-   
-   def initialize(school)
-     @school=school 
-   end 
-   
-   def roster
-     roster={}
-   end 
-   
-   def add_student(name, grade)
-     roster[grade]= []
-     roster[grade] << name 
-   end 
-   
-     
-  
-end 
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
+end
